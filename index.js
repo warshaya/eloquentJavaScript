e21dec9480185443c5157935fb04cc7ff34d1846
warshaya <warshaya@gmail.com>
2018-04-10 13:57:39 -0700

    move graph, roads, and villagestate all into the one index.js file

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 'use strict';
 
-const { roads } = require('./roads.js');
+/**
+* this is the data that makes up a town. a list of connecting roads
+*/
+const roads = [
+  "Alice's House-Bob's House",   "Alice's House-Cabin",
+  "Alice's House-Post Office",   "Bob's House-Town Hall",
+  "Daria's House-Ernie's House", "Daria's House-Town Hall",
+  "Ernie's House-Grete's House", "Grete's House-Farm",
+  "Grete's House-Shop",          "Marketplace-Farm",
+  "Marketplace-Post Office",     "Marketplace-Shop",
+  "Marketplace-Town Hall",       "Shop-Town Hall"
+];
 
 /**
 * Given an array of edges, buildGraph creates a map object that,
@@ -22,5 +33,43 @@ function buildGraph(edges) {
   return graph;
 }
 
+/**
+* This creates a village state.  It has current position of the robot,
+* as well as any remaining packages to deliver, and their destination addresses.
+*
+* If the robot moves to a spot, he will deliver the packages that go there.
+*/
+class VillageState {
+  constructor(place, parcels) {
+    this.place = place;
+    this.parcels = parcels;
+  }
+
+  move(destination) {
+    if (!roadGraph[this.place].includes(destination)) {
+      return this;
+    } else {
+      let parcels = this.parcels.map(p => {
+        if (p.place != this.place) return p;
+        return {place: destination, address: p.address};
+      }).filter(p => p.place != p.address);
+      return new VillageState(destination, parcels);
+    }
+  }
+}
+
 const roadGraph = buildGraph(roads);
+
+let first = new VillageState(
+  "Post Office",
+  [{place: "Post Office", address: "Alice's House"}]
+);
+let next = first.move("Alice's House");
+
+console.log(next.place);
+// → Alice's House
+console.log(next.parcels);
+// → []
+console.log(first.place);
+// → Post Office
 
