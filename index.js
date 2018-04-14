79f570768056d9f0f5b669e048e4a03ea5c9e7ef
warshaya <warshaya@gmail.com>
2018-04-14 09:34:56 -0700

    adding the robot to the app

--- index.js
+++ index.js
@@ -73,3 +73,31 @@ console.log(next.parcels);
 console.log(first.place);
 // → Post Office
 
+/**
+* This has the robot take a step, move packages, and deliver them.
+*/
+function runRobot(state, robot, memory) {
+  for (let turn = 0;; turn++) {
+    if (state.parcels.length == 0) {
+      console.log(`Done in ${turn} turns`);
+      break;
+    }
+    let action = robot(state, memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+    console.log(`Moved to ${action.direction}`);
+  }
+}
+
+/**
+* A dumb robot moves in a random direction
+*/
+function randomPick(array) {
+  let choice = Math.floor(Math.random() * array.length);
+  return array[choice];
+}
+
+function randomRobot(state) {
+  return {direction: randomPick(roadGraph[state.place])};
+}
+
