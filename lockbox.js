3de684224dedcb279c7591394df6e1a4ac438ac0
warshaya <warshaya@gmail.com>
2018-05-13 08:42:07 -0700

    +lockbox.js, lockbox.test.js, create lockbox and tests for it

---
+++ lockbox.js
@@ -0,0 +1,18 @@
+
+const box = {
+  locked: true,
+  unlock() { this.locked = false; },
+  lock() { this.locked = true;  },
+  _content: [],
+  get content() {
+    if (this.locked) throw new Error("Locked!");
+    return this._content;
+  }
+};
+
+function withBoxUnlocked(body) {
+  // Your code here.
+}
+
+module.exports = { box, withBoxUnlocked };
+
---
+++ lockbox.test.js
@@ -0,0 +1,5 @@
+
+const { box, withBoxUnlocked } = require('./lockbox.js');
+
+
+
